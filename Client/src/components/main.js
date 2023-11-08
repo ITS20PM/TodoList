076ecee8df6b4efa8f9180e916b32ee6574f77c1
ITS20PM <patrick21meng@gmail.com>
2023-11-08 08:43:55 -0800

    finish pass down props and form functionality

--- Client/src/components/main.js
+++ Client/src/components/main.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../forms/main';
 import {Container} from './style';
 
 function Todo() {
+    const [input, setInput] = useState('');
+    console.log(input, "input");
+
     return <Container>
             <h2>List of Todos</h2>
+            
+            <Form input={input} setInput={setInput}/>
         </Container>
 }
 
